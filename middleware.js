ca51a4ccfc0844c684f5aac46d1599b0f736a20e
qoribulmujib <muhamadqoribulmujib@gmail.com>
2024-12-31 17:12:30 +0700

    🐛 fix: issue image not showing

--- middleware.js
+++ middleware.js
@@ -18,5 +18,5 @@ export function middleware(request) {
 // See "Matching Paths" below to learn more
 export const config = {
   matcher:
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/((?!api|_next/static|favicon.ico|sitemap.xml|robots.txt).*)",
 };
